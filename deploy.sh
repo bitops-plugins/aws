ce1a2c8f61db10dadeb3188cb3b6bc663821c74d
Shyam <shyam.rayaprolu@gmail.com>
2022-02-25 22:36:23 +0530

    updated aws session setup

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,34 @@
 #!/usr/bin/env bash
 set -e
 
+echo "Creating AWS Profile"
+
+if [ -z "$AWS_ACCESS_KEY_ID" ]; then
+  printf "${ERROR}environment variable (AWS_ACCESS_KEY_ID) not set ${NC}"
+  exit 1
+fi
+if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
+  printf "${ERROR}environment variable (AWS_SECRET_ACCESS_KEY) not set ${NC}"
+  exit 1
+fi
+if [ -z "$AWS_DEFAULT_REGION" ]; then
+  printf "${ERROR}environment variable (AWS_DEFAULT_REGION) not set ${NC}"
+  exit 1
+fi
+
+
+mkdir -p /root/.aws
+cat <<EOF > /root/.aws/credentials
+[default]
+aws_access_key_id = "${AWS_ACCESS_KEY_ID}"
+aws_secret_access_key = "${AWS_SECRET_ACCESS_KEY}"
+EOF
+
+cat <<EOF > /root/.aws/config
+[default]
+region = "$AWS_DEFAULT_REGION"
+output = json
+EOF
+
 echo "aws sts get-caller-identity"
 aws sts get-caller-identity
